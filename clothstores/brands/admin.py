0036667ea79e7f3c74f1f050cbab0d0fa01e2098
sam <satish@pytheroem.com>
2021-06-28 09:40:35 +0530

    initial commit.

--- clothstores/brands/admin.py
+++ clothstores/brands/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import customer
 # Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display =['name','add']
+admin.site.register(customer,CustomerAdmin)
--- clothstores/brands/models.py
+++ clothstores/brands/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class customer(models.Model):
+    name=models.CharField(max_length=20)
+    add=models.CharField(max_length=45)
